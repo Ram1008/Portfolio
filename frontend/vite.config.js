82b3dd83d967edc1900cdd2822320ab2328efc11
Ram1008 <ram8176970175@gmail.com>
2024-10-10 19:28:15 +0530

    minor bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true, // Automatically open the browser on server start
-  },
-  resolve: {
-    alias: {
-      // Set up path aliases if needed
-      '@': '/src',
-    },
+    open: true, 
   },
 })
